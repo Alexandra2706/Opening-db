5ef2a0a4cb08cc478ab4595b08269333aa477111
Александра Гаврилова <cx_76@mail.ru>
2024-08-24 00:03:04 +0300

    fix test

--- api/structures/status_test.go
+++ api/structures/status_test.go
@@ -1,20 +1,21 @@
 package structures
 
 import (
-	"log"
 	"testing"
 )
 
 func TestUnmarshalJSON(t *testing.T) {
 	data := []byte("\"released\"")
-	log.Println("data=", data)
-	log.Println("data=", string(data))
 
 	v := &ValidatableEnum[AnimeStatus]{}
-	log.Println("v0=", v)
 	err := v.UnmarshalJSON(data)
-	log.Println("v1=", v)
 	if err != nil {
 		t.Errorf("Should not produce an error, but have one: %q", err.Error())
+	}
+
+	data = []byte("\"new format\"")
+	err = v.UnmarshalJSON(data)
+	if err == nil {
+		t.Errorf("Must be error")
 	}
 }
